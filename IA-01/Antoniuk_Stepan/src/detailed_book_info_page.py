fe4949d88713d08cb8a90a84fdda73a1c2168f79
SanAntonik <sanantonik@gmail.com>
2023-01-01 15:11:33 +0200

    Change how you present book info

--- IA-01/Antoniuk_Stepan/src/detailed_book_info_page.py
+++ IA-01/Antoniuk_Stepan/src/detailed_book_info_page.py
@@ -7,7 +7,7 @@ from database_handling import get_every_book_from_DB
 
 def show_detailed_book_info_page():
     st.write("### Here you can see more info about the book you chose")
-    st.write("To see the info, we need your book's ID")
+    st.write("To see the info, we need your chosen book's ID")
 
     engine = establish_connection()
     df = get_every_book_from_DB(engine)
@@ -25,7 +25,20 @@ def show_detailed_book_info_page():
                 # need to subract 1 because it shows the book with
                 # incorrect by +1 ID without it
                 book_id -= 1
-                book_info = df.iloc[book_id]
-                st.write(book_info)
+                chosen_book_info = df.iloc[book_id]
+
+                title, description, author, rating = chosen_book_info[:4]
+                rating_count, reg_price, audio_len, language = chosen_book_info[4:]
+
+                st.write("")
+                st.write("")
+                st.write(f"##### Title: {title}")
+                st.write(f"##### Description: {description}")
+                st.write(f"##### Author: {author}")
+                st.write(f"##### Rating: {rating}")
+                st.write(f"##### Rating count: {rating_count}")
+                st.write(f"##### Regular price: {reg_price}")
+                st.write(f"##### Audio length: {audio_len}")
+                st.write(f"##### Language: {language}")
             except IndexError:
                 st.write("The book with this ID isn't present in DB")
