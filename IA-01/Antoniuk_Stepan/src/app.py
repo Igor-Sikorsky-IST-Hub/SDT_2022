63eb0dfe0a5b5b3a8946dea94c1f7dcc507588ca
SanAntonik <sanantonik@gmail.com>
2023-01-02 11:16:59 +0200

    Slightly restructure app.py

--- IA-01/Antoniuk_Stepan/src/app.py
+++ IA-01/Antoniuk_Stepan/src/app.py
@@ -1,20 +1,28 @@
 import streamlit as st
 
+
 from scrape_page import show_scrape_page
 from all_books_page import show_all_books_page
 from detailed_book_info_page import show_detailed_book_info_page
 
-st.title("Audible Web Scraper")
 
-page = st.sidebar.selectbox("Choose page",
-                            ("Scrape", "See all books",
-                             "See wish list",
-                             "See detailed book info"))
-if page == "Scrape":
-    show_scrape_page()
-elif page == "See all books":
-    show_all_books_page()
-elif page == "See wish list":
-    pass
-elif page == "See detailed book info":
-    show_detailed_book_info_page()
+def main():
+    st.title("Audible Web Scraper")
+
+    page = st.sidebar.selectbox("Choose page",
+                                ("Scrape webpage",
+                                 "See all books",
+                                 "See wish list",
+                                 "See detailed book info"))
+    if page == "Scrape webpage":
+        show_scrape_page()
+    elif page == "See all books":
+        show_all_books_page()
+    elif page == "See wish list":
+        pass
+    elif page == "See detailed book info":
+        show_detailed_book_info_page()
+
+
+if __name__ == "__main__":
+    main()
