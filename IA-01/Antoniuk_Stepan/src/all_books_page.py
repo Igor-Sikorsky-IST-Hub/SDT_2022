114d5a65e47662e38de773edaaa0c4981dcfb391
SanAntonik <sanantonik@gmail.com>
2023-01-02 11:30:01 +0200

    Remove several unimformative cols before showing all books

--- IA-01/Antoniuk_Stepan/src/all_books_page.py
+++ IA-01/Antoniuk_Stepan/src/all_books_page.py
@@ -7,8 +7,14 @@ from database_handling import get_every_book_from_DB
 def show_all_books_page():
     engine = establish_connection()
     df = get_every_book_from_DB(engine)
-    st.write("#### Here are all your scraped books")
-    st.dataframe(df)
+    st.write("### Here are all your scraped books")
+    # remove several not much useful cols
+    # from your df before showing it
+    df_shortened = df.drop(columns=["description",
+                                    "language",
+                                    "page_URL",
+                                    "cover_image_URL"])
+    st.dataframe(df_shortened)
 
     # Maybe add the possibility to add books to wish list here
     pass
